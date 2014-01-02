284236f4a7636e20ecb42a51e0be86a7adb689d0
TuncayCiftci <tuncay@mail.com>
2014-01-02 16:39:47 +0200

    rails deneme; bootstrap eklemesi yapıldı.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ciftci::Application.config.secret_key_base = 'e8e83bec6325cf249b1b161f759dcc0fc6906ee0fa4665ce3fc463e5b7a1e2d40d1c3f9743c5c780327aee36500204644a4ecc0668a5e7cbb145b1287f1a3f2c'
