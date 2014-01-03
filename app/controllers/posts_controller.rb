c844fa9a9c7e371ba835634981881ca4fe7e6b8a
TuncayCiftci <tuncay@mail.com>
2014-01-03 11:26:04 +0200

    sedat bi bak

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,14 +61,26 @@ class PostsController < ApplicationController
     end
   end
 
+def tagged
+    if params[:tag].present? 
+      @posts = Post.tagged_with(params[:tag])
+    else 
+      @posts = Post.postall
+    end  
+  end
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
+
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :tag_list)
     end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+
+acts_as_taggable   
+   acts_as_taggable_on :tags
 end
